df6610208d6d536e34d72e0291e8c1916ec27e0f
Samuel Reichert <samuelreichert@Samuels-MacBook-Pro.local>
2023-02-21 00:29:17 +0100

    Update cypress tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,9 +1,10 @@
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+describe('Home page', () => {
+  it('should see jokes on home page', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
 
     cy.get('h2').contains('Jokes')
+    cy.get('p[data-testid="joke-item"]').should('have.length', 10)
   })
 })
 
