7e4760aab438daab637a4a4c521a9b7c9758c827
Samuel Reichert <samuelreichert@Samuels-MacBook-Pro.local>
2023-02-22 12:10:11 +0100

    Remove unused router from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,8 @@
 import { FC } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { styled } from '@/stitches.config'
 
 export const Navbar: FC = () => {
-  const router = useRouter()
-  console.log(router)
   return (
     <Nav>
       <NavItem href="/">Home</NavItem>
